8e7c2395d6a44c80cd2573ce0638ef0e21dd7409
Kadhambari Raghavun <kadhambariraghavun@Kadhambaris-iMac.local>
2019-12-18 17:41:51 -0600

    End of Section 9

--- DatingApp.API/Models/User.cs
+++ DatingApp.API/Models/User.cs
@@ -28,6 +28,8 @@ namespace DatingApp.API.Models
 
         public string Introduction {get; set;}
 
+        public string LookingFor {get; set;}
+
         public string Interests {get; set;}
 
         public string City {get; set;}
